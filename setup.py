35cd6bb160daa0599253a8b0bc005d85f095db97
Shubham Dwivedi <shubhamdwivediconnect@gmail.com>
2025-07-11 02:01:11 -0400

    wheel3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.1.0',
     description='Configurable data validation framework on PySpark',
     author='Shubham Dwivedi',
-    packages=find_packages(include=['validators*']),
+    packages=find_packages(include=['validators*','niyamcore*"']),
     py_modules=['ValidationConfigParser'],
     install_requires=[
         'pyspark>=3.0.0',
