fa3c5c6f5204b4ea4cc5f321d10812e853c5be53
Shubham Dwivedi <shubhamdwivediconnect@gmail.com>
2025-07-12 13:00:25 -0400

    Test_1

--- niyamcore/runner.py
+++ niyamcore/runner.py
@@ -2,5 +2,6 @@ from niyamcore.base_validations import NiyamCoreValidator
 
 # Usage example
 validator = NiyamCoreValidator(df)
-result = validator.not_null_check(['customer_id', 'email'])
-print(result)
+# result = validator.not_null_check(['customer_id', 'email'])
+# print(result)
+print("Hello guysssss!!!!!")
