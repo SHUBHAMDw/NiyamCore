0e81da89bbd633576e670cf03eb3e249d8ea11a4
Shubham Dwivedi <shubhamdwivediconnect@gmail.com>
2025-07-12 13:29:01 -0400

    t3

--- build/lib/niyamcore/runner.py
+++ build/lib/niyamcore/runner.py
@@ -1,6 +1,7 @@
 from niyamcore.base_validations import NiyamCoreValidator
 
 # Usage example
-validator = NiyamCoreValidator(df)
-result = validator.not_null_check(['customer_id', 'email'])
-print(result)
+# validator = NiyamCoreValidator(df)
+# result = validator.not_null_check(['customer_id', 'email'])
+# print(result)
+print("Hello guysssss!!!!!")
